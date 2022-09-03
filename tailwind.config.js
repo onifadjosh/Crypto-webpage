e249f03b8947063f6a3622da58f26f62df932f60
onifadjosh <onifadjosh@gmail.com>
2022-09-03 22:47:39 +0100

    crypto webpage

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'primcol': '#0D0D2B',
+        'faded-white': 'rgba(255, 255, 255, 0.05)',
+
+
+        
+      },
+    },
+  },
+  plugins: [],
+}
