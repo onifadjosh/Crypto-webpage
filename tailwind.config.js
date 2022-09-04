85373419225d76b85ffc79a3cf3c9e44f3b6ccba
onifadjosh <onifadjosh@gmail.com>
2022-09-04 23:05:44 +0100

    site under construction

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,24 @@ module.exports = {
   content: ["./*.{html,js}"],
   theme: {
     extend: {
+
+      fontFamily: {
+       
+        'display': ['Rubik']
+          
+      },
       colors: {
         'primcol': '#0D0D2B',
+        'seccol': '#3671E9',
+        'tetcol': '#2B076E',
         'faded-white': 'rgba(255, 255, 255, 0.05)',
+      },
 
-
-        
+      height: {
+        '1/2-screen': '50vh',
       },
     },
+      
   },
   plugins: [],
 }
